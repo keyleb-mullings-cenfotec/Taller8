532399f65ebc7cafcaabb4a8fe7eb932579ee407
keyleb-mullings-cenfotec <60416684+keyleb-mullings-cenfotec@users.noreply.github.com>
2022-03-17 18:38:15 -0600

    workshop completed

--- src/App.js
+++ src/App.js
@@ -1,22 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import Students from './components/Students';
+import AddStudent from './components/AddStudent';
+import { useState } from 'react';
 
 function App() {
+
+  const initialState = [
+    {
+        id: 1,
+        name: 'Ana',
+        age: 20,
+        grade: 'A',
+    },
+    {
+        id: 2,
+        name: 'Carlos',
+        age: 21,
+        grade: 'B',
+    },
+    {
+        id: 3,
+        name: 'Fabian',
+        age: 30,
+        grade: 'A+',
+    },
+    {
+        id: 4,
+        name: 'Karla',
+        age: 40,
+        grade: 'C',
+    },
+]
+
+const [students, setStudents] = useState(initialState)
+
+const handleNewStudent = student => {
+    setStudents([...students, student])
+};
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Students students={students}/>
+        <AddStudent addNewStudent={handleNewStudent}/>
       </header>
     </div>
   );
